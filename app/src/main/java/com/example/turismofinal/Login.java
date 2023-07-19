ba0ac21aa51f646064aa3f61a96cbcf95da8e4fd
DaniLeon <122113337+Danileon0226@users.noreply.github.com>
2023-07-18 20:42:08 -0500

    ok

--- app/src/main/java/com/example/turismofinal/Login.java
+++ app/src/main/java/com/example/turismofinal/Login.java
@@ -3,8 +3,13 @@ package com.example.turismofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.FillEventHistory;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -12,6 +17,7 @@ public class Login extends AppCompatActivity {
     EditText cajaNombreUsuario;
     EditText cajaContrasena;
 
+    Button botoningreseaqui;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -21,10 +27,32 @@ public class Login extends AppCompatActivity {
 
         cajaNombreUsuario=findViewById(R.id.nombreusuario);
         cajaContrasena=findViewById(R.id.passworUsuario);
+        botoningreseaqui=findViewById(R.id.botoningresar);
 
-    //capturando datos de las cajas (Edittext)
-        String nombreUsuario=cajaNombreUsuario.getText().toString();
-        String Contrasena=cajaContrasena.getText().toString();
+        botoningreseaqui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //capturando datos de las cajas (Edittext)
+                String nombreUsuario=cajaNombreUsuario.getText().toString();
+                String passworUsuario=cajaContrasena.getText().toString();
+
+              //  Toast.makeText(Login.this, "Su nombre es : "+nombreUsuario, Toast.LENGTH_SHORT).show();
+             //   Toast.makeText(Login.this, "Su contraseña es: "+passworUsuario, Toast.LENGTH_SHORT).show();
+
+                //Abrimos una nueva actividad (Home)
+                Intent intent=new Intent(Login. this, Home.class);
+
+                //Pasando datos a la nueva Actividad
+               intent.putExtra("Nombre", nombreUsuario);
+                intent.putExtra("contraseña", passworUsuario);
+
+                startActivity(intent);
+            }
+        });
+
+
+
+
 
     }
 }
